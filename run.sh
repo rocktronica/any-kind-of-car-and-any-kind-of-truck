80e5bb1c92f3764d76fc5645418e5c3316b4673e
Tommy <tommy@iamnotagoodartist.com>
2024-02-12 20:22:06 -0800

    Stub run.sh to compile and emulate
    Works!

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+{
+
+# Exit on error
+set -o errexit
+set -o errtrace
+
+stub="any-kind-of-car-and-any-kind-of-truck"
+
+ardens="/Applications/Ardens/ArdensPlayer.app/Contents/MacOS/ArdensPlayer"
+
+input_path="arduino/$stub"
+build_path="$PWD/build"
+
+mkdir -pv "$build_path"
+
+arduino-cli compile \
+    --fqbn arduboy:avr:arduboy \
+    --build-path="$build_path" \
+    "$input_path"
+
+$ardens file="$build_path/$stub.ino.hex"
+
+}
