83a9e4a769a685dc3e41a3fb94a78c78c767b994
Tommy <tommy@iamnotagoodartist.com>
2024-02-13 14:41:21 -0800

    Stub title and status switcher

--- arduino/any-kind-of-car-and-any-kind-of-truck/any-kind-of-car-and-any-kind-of-truck.ino
+++ arduino/any-kind-of-car-and-any-kind-of-truck/any-kind-of-car-and-any-kind-of-truck.ino
@@ -1,51 +1,56 @@
-/*
-Hello, World! example
-June 11, 2015
-Copyright (C) 2015 David Martinez
-All rights reserved.
-This code is the most basic barebones code for writing a program for Arduboy.
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-*/
-
 #include <Arduboy2.h>
 
-// make an instance of arduboy used for many functions
+enum class GameStatus : uint8_t {
+  TitleScreen,
+  Play,
+};
+
 Arduboy2 arduboy;
 
+GameStatus gameStatus = GameStatus::TitleScreen;
 
-// This function runs once in your game.
-// use it for anything that needs to be set only once in your game.
 void setup() {
-  // initiate arduboy instance
-  arduboy.begin();
-
-  // here we set the framerate to 15, we do not need to run at
-  // default 60 and it saves us battery life
+  arduboy.boot(); // TODO: use .begin() for Arduboy splash
   arduboy.setFrameRate(15);
 }
 
+void titleScreen() {
+  arduboy.setCursor(12, 20);
+  arduboy.print("Any Kind of Car &");
 
-// our main game loop, this runs once every cycle/frame.
-// this is where our game logic goes.
+  arduboy.setCursor(12, 30);
+  arduboy.print("Any Kind of Truck");
+
+  if (arduboy.justPressed(A_BUTTON)) {
+    gameStatus = GameStatus::Play;
+  }
+}
+
+void play() {
+  arduboy.setCursor(12, 20);
+  arduboy.print("play()");
+
+  if (arduboy.justPressed(A_BUTTON)) {
+    gameStatus = GameStatus::TitleScreen;
+  }
+}
+
 void loop() {
-  // pause render until it's time for the next frame
-  if (!(arduboy.nextFrame()))
+  if (!(arduboy.nextFrame())) {
     return;
+  }
 
-  // first we clear our screen to black
+  arduboy.pollButtons();
   arduboy.clear();
 
-  // we set our cursor 5 pixels to the right and 10 down from the top
-  // (positions start at 0, 0)
-  arduboy.setCursor(4, 9);
+  switch (gameStatus) {
+    case GameStatus::TitleScreen:
+      titleScreen();
+      break;
+    case GameStatus::Play:
+      play();
+      break;
+  }
 
-  // then we print to screen what is in the Quotation marks ""
-  arduboy.print(F("Hello, world!"));
-
-  // then we finaly we tell the arduboy to display what we just wrote to the display
   arduboy.display();
 }
