698100a5f663e74a16ea61f7ed67c2ae42b6f344
Tommy <tommy@iamnotagoodartist.com>
2024-02-17 22:24:02 -0800

    Use faster horizontal/vertical drawLines
    Why doesn't it do this under the hood tho?
    
    The +1 on height/width is confusing but I guess
    makes sense. drawLine is inclusive of its two
    points. Their distance is exclusive of one.

--- arduino/any-kind-of-car-and-any-kind-of-truck/utils.h
+++ arduino/any-kind-of-car-and-any-kind-of-truck/utils.h
@@ -8,13 +8,30 @@ struct Xy {
   uint8_t y;
 };
 
-// TODO: use faster horizontal/vertical methods
+void drawLine(
+  int16_t x0,
+  int16_t y0,
+  int16_t x1,
+  int16_t y1,
+  Arduboy2 arduboy
+) {
+  if (x0 == x1 && y0 == y1) {
+    return;
+  } else if (x0 == x1) {
+    arduboy.drawFastVLine(x0, min(y0, y1), abs(y0 - y1) + 1);
+  } else if (y0 == y1) {
+    arduboy.drawFastHLine(min(x0, x1), y0, abs(x0 - x1) + 1);
+  } else {
+    arduboy.drawLine(x0, y0, x1, y1);
+  }
+};
+
 void drawSegmentedLine(Xy points[], uint8_t size, Arduboy2 arduboy) {
   Xy lastPoint = {0,0};
 
   for (int i = 0; i < size; i++) {
     if (i > 0) {
-      arduboy.drawLine(points[i].x, points[i].y, lastPoint.x, lastPoint.y);
+      drawLine(points[i].x, points[i].y, lastPoint.x, lastPoint.y, arduboy);
     }
 
     lastPoint = points[i];
@@ -24,7 +41,7 @@ void drawSegmentedLine(Xy points[], uint8_t size, Arduboy2 arduboy) {
 // TODO: chamfer
 void polygon(Xy points[], uint8_t size, Arduboy2 arduboy) {
   drawSegmentedLine(points, size, arduboy);
-  arduboy.drawLine(points[0].x, points[0].y, points[size - 1].x, points[size - 1].y);
+  drawLine(points[0].x, points[0].y, points[size - 1].x, points[size - 1].y, arduboy);
 };
 
 #endif
