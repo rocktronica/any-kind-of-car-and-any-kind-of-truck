518058486cf87e7ed24c3d2879a2a2b94492944c
Tommy <tommy@iamnotagoodartist.com>
2024-02-17 21:24:24 -0800

    Enable quitting emulator to re-run runner
    Also considered having it re-run on code save
    but seemed too obtrusive

--- run.sh
+++ run.sh
@@ -13,13 +13,45 @@ ardens="/Applications/Ardens/ArdensPlayer.app/Contents/MacOS/ArdensPlayer"
 input_path="arduino/$stub"
 build_path="$PWD/build"
 
-mkdir -pv "$build_path" >/dev/null
+function help() {
+    echo "\
+Compile and emuluate
 
-arduino-cli compile \
-    --fqbn arduboy:avr:arduboy \
-    --build-path="$build_path" \
-    "$input_path"
 
-$ardens file="$build_path/$stub.ino.hex"
+Usage:
+$0
+$0 -h   Show help and exit
+"
+}
+
+if [ "$1" == '-h' ]; then
+    help
+    exit
+fi
+
+function run() {
+    mkdir -pv "$build_path" >/dev/null
+
+    while true; do
+        arduino-cli compile \
+            --fqbn arduboy:avr:arduboy \
+            --build-path="$build_path" \
+            "$input_path"
+
+        $ardens file="$build_path/$stub.ino.hex" >/dev/null
+
+        echo
+        echo "!! Press CTRL+C to quit !!"
+
+        echo
+    done
+}
+
+if [ "$1" == '-h' ]; then
+    help
+    exit
+fi
+
+run "${@:1}"
 
 }
