3962fa2fa127886877d8c84005a193d2fb9ba36c
Tommy <tommy@iamnotagoodartist.com>
2024-02-23 12:45:47 -0800

    Extract "noise"

--- arduino/any-kind-of-car-and-any-kind-of-truck/any-kind-of-car-and-any-kind-of-truck.ino
+++ arduino/any-kind-of-car-and-any-kind-of-truck/any-kind-of-car-and-any-kind-of-truck.ino
@@ -2,6 +2,7 @@
 #include <ArduboyTones.h>
 #include <Tinyfont.h>
 #include "images.h"
+#include "noise.h"
 #include "utils.h"
 #include "vehicle.h"
 
@@ -23,39 +24,6 @@ int8_t jumpFramesElapsed = JUMP_FRAMES_MAX;
 int16_t themePlayingFramesElapsed = 0;
 
 bool showTitleOverlay = true;
-
-// TODO: extract
-// https://onlinesequencer.net/3857464
-const uint16_t themeTones[] PROGMEM = {
- NOTE_G3,34,
- NOTE_C5,136, NOTE_C5,136, NOTE_C5,136, NOTE_C5,136, NOTE_G4,204, NOTE_REST,68, NOTE_G4,136, NOTE_E4,136,
- NOTE_E4,136, NOTE_E4,136, NOTE_E4,136, NOTE_C4,272, NOTE_REST,136, NOTE_C4,136, NOTE_C4,136, NOTE_C4,136,
- NOTE_C4,136, NOTE_C4,136, NOTE_C4,136, NOTE_D4,204, NOTE_REST,68, NOTE_C4,204, NOTE_REST,68, NOTE_C4,204,
- NOTE_REST,204, NOTE_C4,68, NOTE_E4,68, NOTE_C5,136,
- TONES_END
-};
-
-const uint16_t changeTones[] PROGMEM = {
- NOTE_G3,34,
- NOTE_C4,68, NOTE_E4,68, NOTE_C5,136,
- TONES_END
-};
-
-const uint16_t moveTones[] PROGMEM = {
- NOTE_G3,34,
- NOTE_E3,34,
- TONES_END
-};
-
-const uint16_t jumpTones[] PROGMEM = {
- NOTE_E4,34, NOTE_D4,34, NOTE_C4,34,
- TONES_END
-};
-
-const uint16_t crouchTones[] PROGMEM = {
- NOTE_G3,34,NOTE_F3,68,
- TONES_END
-};
 
 void setup() {
   arduboy.beginDoFirst();
