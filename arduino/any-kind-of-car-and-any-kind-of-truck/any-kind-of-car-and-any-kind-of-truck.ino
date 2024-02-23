46a2050c670e95cd23b53deb9d50f86268e3bcb7
Tommy <tommy@iamnotagoodartist.com>
2024-02-22 21:50:12 -0800

    Refine theme, add sfx for jump/crouch

--- arduino/any-kind-of-car-and-any-kind-of-truck/any-kind-of-car-and-any-kind-of-truck.ino
+++ arduino/any-kind-of-car-and-any-kind-of-truck/any-kind-of-car-and-any-kind-of-truck.ino
@@ -20,22 +20,35 @@ int8_t jumpFramesElapsed = JUMP_FRAMES_MAX;
 
 bool showTitleOverlay = true;
 
+// https://onlinesequencer.net/3857464
 const uint16_t themeTones[] PROGMEM = {
- NOTE_C5,136, NOTE_C5,136, NOTE_C5,136, NOTE_C5,136, NOTE_G4,136, NOTE_REST,136, NOTE_G4,136, NOTE_E4,136,
- NOTE_E4,136, NOTE_E4,136, NOTE_E4,136, NOTE_E4,136, NOTE_C4,136, NOTE_REST,136, NOTE_C4,136, NOTE_C4,136,
- NOTE_C4,136, NOTE_C4,136, NOTE_C4,136, NOTE_C4,136, NOTE_D4,136, NOTE_REST,136, NOTE_C4,136, NOTE_REST,136,
- NOTE_C4,136, NOTE_REST,272, NOTE_C4,68, NOTE_E4,68, NOTE_C5,136,
-
+ NOTE_G3,34,
+ NOTE_C5,136, NOTE_C5,136, NOTE_C5,136, NOTE_C5,136, NOTE_G4,204, NOTE_REST,68, NOTE_G4,136, NOTE_E4,136,
+ NOTE_E4,136, NOTE_E4,136, NOTE_E4,136, NOTE_C4,272, NOTE_REST,136, NOTE_C4,136, NOTE_C4,136, NOTE_C4,136,
+ NOTE_C4,136, NOTE_C4,136, NOTE_C4,136, NOTE_D4,204, NOTE_REST,68, NOTE_C4,204, NOTE_REST,68, NOTE_C4,204,
+ NOTE_REST,204, NOTE_C4,68, NOTE_E4,68, NOTE_C5,136,
  TONES_END
 };
 
 const uint16_t changeTones[] PROGMEM = {
+ NOTE_G3,34,
  NOTE_C4,68, NOTE_E4,68, NOTE_C5,136,
  TONES_END
 };
 
 const uint16_t moveTones[] PROGMEM = {
- NOTE_G3,34, NOTE_E3,34,
+ NOTE_G3,34,
+ NOTE_E3,34,
+ TONES_END
+};
+
+const uint16_t jumpTones[] PROGMEM = {
+ NOTE_E4,34, NOTE_D4,34, NOTE_C4,34,
+ TONES_END
+};
+
+const uint16_t crouchTones[] PROGMEM = {
+ NOTE_G3,34,NOTE_F3,68,
  TONES_END
 };
 
@@ -79,6 +92,9 @@ void play() {
     jumpFramesElapsed = jumpFramesElapsed + 1;
   } else if (arduboy.pressed(UP_BUTTON)) {
     jumpFramesElapsed = 0;
+    sound.tones(jumpTones);
+  } else if (arduboy.pressed(DOWN_BUTTON)) {
+    sound.tones(crouchTones);
   }
 
   vehicle.setCrouch(
